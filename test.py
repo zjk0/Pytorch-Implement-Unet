e5d3bbf6390c99b13269f7e86ff3a081223c9cec
zjk0 <2054295479@qq.com>
2025-04-29 23:50:48 +0800

    second commit

--- test.py
+++
@@ -1,5 +0,0 @@
-import os
-
-dir = "./lung_seg_data/img_train"
-files = os.listdir(dir)
-print(len(files))
--- unet.py
+++ unet.py
@@ -1,24 +1,18 @@
-import argparse
 import os
-
 import cv2 as cv
 import numpy as np
-
-import tkinter as tk
-from tkinter import ttk
-from tkinter import filedialog as fd
-from PIL import ImageTk
-
 import torch
 import torch.nn as nn
-from torch.optim import Adam
+import torch.optim as optim
+from torch.optim.lr_scheduler import StepLR
 from torch.utils.data import Dataset
 from torch.utils.data import DataLoader
+from matplotlib import pyplot as plt
 
 '''
 @brief 训练集的类
 '''
-class LungDataset (Dataset):
+class TrainDataset (Dataset):
     def __init__ (self, data_train_dir, label_train_dir):
         super().__init__()
 
@@ -107,7 +101,7 @@ class UnetEncoderBlock (nn.Module):
     def forward (self, input):
         output = self.block(input)
         output_pool = self.pool(output)
-        return output, output_pool
+        return output, output_pool  # 前者用于跳跃连接，后者用于下一层的输入
 
 '''
 @brief Unet解码器的一层的类
@@ -190,45 +184,66 @@ class Unet (nn.Module):
 
         return final_trans_output
 
-def train (module, dataloader, device):
-    module.to(device)
-    optimizer = Adam(params = module.parameters(), lr = 0.001)
-    loss_func = nn.CrossEntropyLoss()
+'''
+@brief 模型训练
+@param model: 需要训练的模型
+@param dataloader: 数据加载器
+@param optimizer: 优化器
+@param device: 运行设备
+@return loss_list: 一轮epoch的损失函数列表
+'''
+def train (model, dataloader, optimizer, device):
+    model.to(device)
+    loss_func = nn.CrossEntropyLoss()  # 损失函数为交叉熵
+    loss_list = []
 
     for inputs, targets in dataloader:
         inputs = inputs.to(device)
         targets = targets.to(device)
 
         optimizer.zero_grad()
-        outputs = module(inputs)
+        outputs = model(inputs)
         loss = loss_func(outputs, targets)
-        loss.backward()
+        loss.backward()  # 反向传播
         optimizer.step()
 
+        print(f"当前学习率：{scheduler.get_last_lr()[0]}")
         print(f"损失函数计算值：{loss.item()}")
+        loss_list.append(loss.item())
 
-def test (module, dataloader, device):
-    module.eval()
-    outputs = []
-    file_names = []
+    return loss_list
 
-    with torch.no_grad():
+'''
+@brief 模型测试
+@param model: 需要测试的模型
+@param dataloader: 数据加载器
+@param device: 运行设备
+@return outputs: 测试结果列表
+@return file_names: 测试文件的文件名
+'''
+def test (model, dataloader, device):
+    model.eval()  # 测试模式
+    outputs = []  # 输出值列表
+    file_names = []  # 用于测试的文件名的列表
+
+    with torch.no_grad():  # 没有梯度计算
         for inputs, file_name in dataloader:
             inputs = inputs.to(device)
-            output = module(inputs)
+            output = model(inputs)
             outputs.append(output)
             file_names.append(file_name)
 
     return outputs, file_names
 
 if __name__ == '__main__':   
-    epoches = 5  # 训练次数
+    epoches = 50  # 训练次数
     batch_size = 4  # 批大小
+    loss_lists = []  # 保存每轮epoch的loss列表的列表
 
     # 创建训练集的加载器
     data_train_dir = "./lung_seg_data/img_train"
     label_train_dir = "./lung_seg_data/lab_train"
-    lung_data = LungDataset(data_train_dir, label_train_dir)
+    lung_data = TrainDataset(data_train_dir, label_train_dir)
     lung_dataloader = DataLoader(lung_data, batch_size = batch_size, shuffle = True)
     
     # 创建测试集的加载器
@@ -239,32 +254,57 @@ if __name__ == '__main__':
     # 创建一个Unet
     u_net = Unet()
 
+    # Adam优化器
+    optimizer = optim.Adam(params = u_net.parameters(), lr = 0.001)
+
+    # 学习率规划器
+    scheduler = StepLR(optimizer, 10, 0.5)
+
     # 模型运行设备
     device = "cuda" if torch.cuda.is_available() else "cpu"
 
     # 训练
     for epoch in range(epoches):
-        train(u_net, lung_dataloader, device)
+        loss_list = train(u_net, lung_dataloader, optimizer, device)
+        scheduler.step()  # 规划学习率
+        loss_lists.append(loss_list)
         torch.cuda.empty_cache()  # 清空缓存
         print(f"完成第{epoch + 1}次训练")
 
+    # 测试
     print("正在测试")
     test_output, test_file_name = test(u_net, test_dataloader, device)
     print("完成测试")
 
+    # 对测试结果进行处理，得到分割结果
     test_output = torch.cat(test_output, dim = 0)
     test_result = torch.argmax(test_output, dim = 1)
     output_array = test_result.cpu().numpy()
     output_array = output_array * 255
     output_array = output_array.astype(np.uint8)
 
+    # 测试结果保存的路径
     test_result_dir = "./test_result"
     os.makedirs(test_result_dir, exist_ok = True)
 
+    # 由于batch_size是4，test函数返回的列表的元素是一个长度为4的元组，现在将元素改为单个文件名
     test_file_name = [name for batch_size in test_file_name for name in batch_size]
+
+    # 写入测试结果
     print("正在写入图片")
     for i in range(output_array.shape[0]):
         path = os.path.join(test_result_dir, test_file_name[i])
         cv.imwrite(path, output_array[i, :, :])
     
     print("写入完成")
+
+    # 保存模型
+    save_path = "./unet_model.pth"
+    torch.save(u_net.state_dict(), save_path)
+
+    # 绘制每个epoch的loss曲线，画在一张图中
+    for i in range(epoches):
+        t = range(len(loss_lists[i]))
+        plt.plot(t, loss_lists[i])
+
+    plt.show()
